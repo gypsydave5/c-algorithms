67d99afaa047af9f71624ead22872fbf1b0f7083
David Wickes <dave.wickes@gmail.com>
2016-04-23 20:32:13 +0100

    microseconds

--- assignment1/smallFib/benchmark.c
+++ assignment1/smallFib/benchmark.c
@@ -12,12 +12,13 @@ long long benchmark(long long (*f)(int), int iterations) {
   }
 
   endTime = (long long)clock();
-  return endTime - startTime;
+  return (endTime - startTime) * 1000000 / CLOCKS_PER_SEC;
 }
 
 int main() {
   long long t1, t2;
   t1 = benchmark(smallFibRecursion, 20000);
   t2 = benchmark(smallFibArray, 20000);
-  printf("RecursionTime: %lld\nArrayTime: %lld\n", t1, t2);
+  printf("RecursionTime: %lldmicroseconds\n", t1);
+  printf("ArrayTime: %lldmicroseconds\n", t1);
 }
