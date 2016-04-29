07583c44291bd71d48b9036ab4cb12f437b2a1c5
David Wickes <dave.wickes@gmail.com>
2016-04-29 14:28:59 +0100

    submitted code - problem sets complete!

--- assignment1/lcm/main.c
+++ assignment1/lcm/main.c
@@ -1,13 +1,22 @@
-#include "lcmEuclid.h"
-#include "lcmNaive.h"
 #include <stdio.h>
+unsigned long long lcmEuclid(unsigned long long a, unsigned long long b);
+unsigned long long gcdEuclid(unsigned long long a, unsigned long long b);
 
 int main() {
-  unsigned long long a, b, result1, result2;
-
+  unsigned long long a, b, result;
   scanf("%lld %lld", &a, &b);
-  result1 = naiveLCM(a, b);
-  result2 = lcmEuclid(a, b);
-  printf("%lld\n", result1);
-  printf("%lld\n", result2);
+  result = lcmEuclid(a, b);
+  printf("%lld\n", result);
+}
+
+unsigned long long lcmEuclid(unsigned long long a, unsigned long long b) {
+  return (a * b) / gcdEuclid(a, b);
+}
+
+unsigned long long gcdEuclid(unsigned long long a, unsigned long long b) {
+  if (b == 0) {
+    return a;
+  }
+  a = a % b;
+  return gcdEuclid(b, a);
 }
