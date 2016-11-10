756ea42025934f886e4f08b9742795b43c8337b6
David Wickes <dave.wickes@gmail.com>
2016-11-10 08:26:33 +0000

    wip

--- graph-algorithms/assignment4/dijkstra/dijkstra.cpp
+++ graph-algorithms/assignment4/dijkstra/dijkstra.cpp
@@ -7,11 +7,55 @@ using std::queue;
 using std::pair;
 using std::priority_queue;
 
+class v_dist {
+public:
+    int distance;
+    int vertex;
+};
+
+bool operator>(const v_dist &lhs, const v_dist &rhs) {
+    return lhs.distance > rhs.distance;
+}
+
 int distance(vector<vector<int>> &adj, vector<vector<int>> &cost, int s,
              int t) {
-  priority_queue<int, vector<int>, std::greater<int>> pq;
+
+  priority_queue<v_dist, vector<v_dist>, std::greater<v_dist>> pq;
+  vector<int> dist(adj.size(), INT_MAX);
+  vector<int> prev(adj.size(), NULL);
+  dist[s] = 0;
+
+  std::cout << "s: " << s << ", t: " << t << '\n';
+
+  for (int i = 0; i < adj.size(); i++) {
+      v_dist vd;
+      vd.distance = dist[i];
+      vd.vertex = i;
+      pq.push(vd);
+  }
+
+  while (pq.size() > 0) {
+      v_dist vd = pq.top();
+      pq.pop();
+
+      for (int i = 0; i < adj[vd.vertex].size(); i++) {
+          int u = vd.vertex;
+          int v = adj[u][i];
+          std::cout << "u: " << u << ", v: " << v << '\n';
+          if (dist[v] > dist[u] + cost[u][i]) {
+              dist[v] = dist[u] + cost[u][v];
+              prev[v] = u;
+
+              v_dist vd;
+              vd.distance = dist[v];
+              vd.vertex = v;
+              pq.push(vd);
+          }
+      }
+  }
+
   // write your code her
-  return -1;
+  return dist[t];
 }
 
 int main() {
