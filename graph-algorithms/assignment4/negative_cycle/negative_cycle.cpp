2a80284668d8ac8c0b0e61ed7d9ba7ee57c84d2c
David Wickes <dave.wickes@gmail.com>
2016-11-11 21:58:36 +0000

    found negative cycles

--- graph-algorithms/assignment4/negative_cycle/negative_cycle.cpp
+++ graph-algorithms/assignment4/negative_cycle/negative_cycle.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
 #include <vector>
+#define MAX_DIST 10001
 
 using std::vector;
 
 int negative_cycle(vector<vector<int> > &adj, vector<vector<int> > &cost) {
-  //write your code here
+  vector<int> dist(adj.size(), MAX_DIST);
+
+  for (int i = 0; i < adj.size() - 1; i++) {
+      for (int u = 0; u < adj.size(); u++) {
+
+          for (int k = 0; k < adj[u].size(); k++) {
+
+              int v = adj[u][k];
+
+              if (dist[v] > dist[u] + cost[u][k]) {
+                  dist[v] = dist[u] + cost[u][k];
+              }
+          }
+      }
+  }
+
+  for (int u = 0; u < adj.size(); u++) {
+      for (int k = 0; k < adj[u].size(); k++) {
+          int v = adj[u][k];
+          int w = cost[u][k];
+          if (dist[u] + w < dist[v]) {
+              return 1;
+          }
+      }
+  }
+
   return 0;
 }
 
@@ -19,5 +45,5 @@ int main() {
     adj[x - 1].push_back(y - 1);
     cost[x - 1].push_back(w);
   }
-  std::cout << negative_cycle(adj, cost);
+  std::cout << negative_cycle(adj, cost) << '\n';
 }
