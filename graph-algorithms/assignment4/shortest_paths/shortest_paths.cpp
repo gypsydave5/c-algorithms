11d358dfed179fbc6cf22a76ae56a34133a5f949
David Wickes <dave.wickes@gmail.com>
2016-11-12 17:10:53 +0000

    getting there - need to pass another test

--- graph-algorithms/assignment4/shortest_paths/shortest_paths.cpp
+++ graph-algorithms/assignment4/shortest_paths/shortest_paths.cpp
@@ -8,8 +8,56 @@ using std::queue;
 using std::pair;
 using std::priority_queue;
 
-void shortest_paths(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, vector<long long> &distance, vector<int> &reachable, vector<int> &shortest) {
-  //write your code here
+void shortest_paths(
+    vector<vector<int> > &adj,
+    vector<vector<int> > &cost,
+    int s,
+    vector<long long> &distance,
+    vector<int> &reachable,
+    vector<int> &shortest
+    ) {
+
+  distance[s] = 0;
+  reachable[s] = 1;
+
+  for (int i = 0; i < adj.size() - 1; i++) {
+      for (int u = 0; u < adj.size(); u++) {
+
+          for (int k = 0; k < adj[u].size(); k++) {
+
+              int v = adj[u][k];
+              int w = cost[u][k];
+
+              if (distance[v] > distance[u] + w) {
+                  reachable[v] = 1;
+                  distance[v] = distance[u] + w;
+              }
+          }
+      }
+  }
+
+  bool changed = true;
+
+  while (changed) {
+      changed = false;
+
+      for (int u = 0; u < adj.size(); u++) {
+
+          for (int k = 0; k < adj[u].size(); k++) {
+              int v = adj[u][k];
+              int w = cost[u][k];
+
+              if (distance[v] > distance[u] + w) {
+                  if (shortest[v]) {
+                      distance[v] = distance[u] + w;
+                      changed = true;
+                  }
+                  shortest[v] = 0;
+              }
+          }
+      }
+  }
+
 }
 
 int main() {
@@ -17,17 +65,21 @@ int main() {
   std::cin >> n >> m;
   vector<vector<int> > adj(n, vector<int>());
   vector<vector<int> > cost(n, vector<int>());
+
   for (int i = 0; i < m; i++) {
     int x, y, w;
     std::cin >> x >> y >> w;
     adj[x - 1].push_back(y - 1);
     cost[x - 1].push_back(w);
   }
+
   std::cin >> s;
   s--;
+
   vector<long long> distance(n, std::numeric_limits<long long>::max());
   vector<int> reachable(n, 0);
   vector<int> shortest(n, 1);
+
   shortest_paths(adj, cost, s, distance, reachable, shortest);
   for (int i = 0; i < n; i++) {
     if (!reachable[i]) {
