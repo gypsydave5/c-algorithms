e155ccd4a05935fd2c3880bd662706cd893e4ccd
David Wickes <dave.wickes@gmail.com>
2016-11-27 13:22:15 +0000

    Passes the connecting points problem

--- graph-algorithms/assignment5/connecting_points/connecting_points.cpp
+++ graph-algorithms/assignment5/connecting_points/connecting_points.cpp
@@ -2,15 +2,76 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <queue>
 #include <cmath>
 
 using std::vector;
+using std::priority_queue;
 
+// This is an edge, measuing the distance from a vertex in X to a vertex in P - X
+// (where P is all the vertices, and X is all the vertices in our MST)
+// Oddly enough, we don't need the vertex it comes from, only the one it goes to
+class edge {
+public:
+    double distance;
+    int vertex;
+};
+
+// Just for sorting the edges by distance
+bool operator>(const edge &lhs, const edge &rhs) {
+    return lhs.distance > rhs.distance;
+}
+
+// Distance between two vertices (which are points in this example. As the data
+// we're getting is as a list of x and a list of y coordinates, we'll just pass
+// both of them in with the index number of each point we're comparing.
+double distance(vector<int> x, vector<int> y, int a, int b) {
+    int x_diff = (x[a] - x[b]);
+    int y_diff = (y[a] - y[b]);
+    return std::sqrt((x_diff * x_diff) + (y_diff * y_diff));
+};
+
+// This bit does the work.
 double minimum_distance(vector<int> x, vector<int> y) {
   double result = 0.;
-  //write your code here
+
+  priority_queue<edge, vector<edge>, std::greater<edge>> pq;
+  vector<bool> in_set(x.size(), false);
+  in_set[0] = true;
+
+  for (size_t i = 1; i < x.size(); i++) {
+      edge e;
+      e.distance = distance(x, y, 0, i);
+      e.vertex = i;
+      pq.push(e);
+  }
+
+  while (pq.size() > 0) {
+      edge e = pq.top();
+      pq.pop();
+      int u = e.vertex;
+
+      if (in_set[u]) {
+          continue;
+      }
+
+      in_set[u] = true;
+      result += e.distance;
+
+      for (int i = 1; i < x.size(); i++) {
+          if (in_set[i]) {
+              continue;
+          }
+
+          edge e;
+          e.distance = distance(x, y, u, i);
+          e.vertex = i;
+          pq.push(e);
+      }
+  }
+
   return result;
-} 
+}
 
 int main() {
   size_t n;
@@ -19,5 +80,5 @@ int main() {
   for (size_t i = 0; i < n; i++) {
     std::cin >> x[i] >> y[i];
   }
-  std::cout << std::setprecision(10) << minimum_distance(x, y) << std::endl;
+  std::cout << std::fixed << std::setprecision(9) << minimum_distance(x, y) << std::endl;
 }
---
+++ graph-algorithms/assignment5/connecting_points/test.t
@@ -0,0 +1,15 @@
+  $ echo "4
+  > 0 0
+  > 0 1
+  > 1 0
+  > 1 1" | $TESTDIR/connecting_points.bin
+  3.000000000
+
+  $ echo "5
+  > 0 0
+  > 0 2
+  > 1 1
+  > 3 0
+  > 3 2" | $TESTDIR/connecting_points.bin
+  7.064495102
+
