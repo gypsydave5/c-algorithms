357b829e438f3cbdf12f857da859885ea87101a9
David Wickes <dave.wickes@gmail.com>
2017-04-05 14:39:18 +0100

    more modular, with some caching

--- string-algorithms/assignment2/bwtinverse/bwtinverse.cpp
+++ string-algorithms/assignment2/bwtinverse/bwtinverse.cpp
@@ -9,32 +9,75 @@ using std::endl;
 using std::string;
 using std::vector;
 
-string InverseBWT(const string& bwt) {
-  string text = "";
+int first_start = -1;
+int a_start = -1;
+int t_start = -1;
+int c_start = -1;
+int g_start = -1;
+
+void rank_sorted(const string& bwt) {
   string sorted = bwt;
-  sort(sorted.begin(), sorted.end());
+  std::sort(sorted.begin(), sorted.end());
 
-  char c = '$';
-  int char_num = 0;
-
-  while (text.length() < bwt.length()) {
-    for (int i = 0; i < bwt.length(); i++) {
-      if (bwt[i] == c) {
-        if (char_num == 0) {
-          text.push_back(c);
-
-          c = sorted[i];
-
-          for (int j = 0; j <= i; j++) {
-            if (sorted[j] == c) {
-              char_num++;
-            }
-          }
-        } else {
-          char_num--;
-        }
-      }
+  for (int i = 0; i < sorted.length(); ++i) {
+    if (first_start == -1 && sorted[i] == '$') {
+      first_start = i;
     }
+    if (a_start == -1 && sorted[i] == 'A') {
+      a_start = i;
+    }
+    if (t_start == -1 && sorted[i] == 'T') {
+      t_start = i;
+    }
+    if (c_start == -1 && sorted[i] == 'C') {
+      c_start = i;
+    }
+    if (g_start == -1 && sorted[i] == 'G') {
+      g_start = i;
+    }
+  }
+}
+
+int rank_of(char c) {
+  switch (c) {
+    case 'A':
+      return a_start;
+    case 'T':
+      return t_start;
+    case 'C':
+      return c_start;
+    case 'G':
+      return g_start;
+    default:
+      return first_start;
+  }
+}
+
+int occurance_up_to(char c, const string& bwt, int pos) {
+  int count = 0;
+  for (int i = 0; i < pos; ++i) {
+    if (bwt[i] == c) {
+      count++;
+    }
+  }
+  return count;
+}
+
+int inverse(int pos, const string& bwt) {
+  char c = bwt[pos];
+  return rank_of(c) + occurance_up_to(c, bwt, pos);
+}
+
+string InverseBWT(const string& bwt) {
+  rank_sorted(bwt);
+
+  string text = "$";
+
+  int pos = 0;
+
+  for (int i = 1; i < bwt.length(); ++i) {
+    text = bwt[pos] + text;
+    pos = inverse(pos, bwt);
   }
 
   return text;
