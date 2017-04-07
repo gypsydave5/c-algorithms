e8bdca7954dc138cffe2cb16bfbec9cef732f71b
David Wickes <dave.wickes@gmail.com>
2017-04-07 18:44:27 +0100

    WIP

--- string-algorithms/assignment2/bwtinverse/c/main.c
+++ string-algorithms/assignment2/bwtinverse/c/main.c
@@ -5,12 +5,15 @@
 
 int main() {
   char read_in[1000000];
-  char c = '\0';
+  int count[256] = {0};
+
+  unsigned char c = '\0';
   int bwt_length = 0;
 
   while (c != '\n') {
     c = getc(stdin);
     read_in[bwt_length] = c;
+    count[c]++;
     bwt_length++;
   }
 
@@ -22,6 +25,15 @@ int main() {
     sorted[i] = read_in[i];
   }
 
+  for (int c = 0; c <= 255; ++c) {
+    int sorted_index = 0;
+    for (int j = 0; j < count[c]; ++j) {
+      sorted[sorted_index] = c;
+      sorted_index++;
+    }
+  }
+
   printf("%s", read_in);
   printf("%s", bwt);
+  printf("%s", sorted);
 }
