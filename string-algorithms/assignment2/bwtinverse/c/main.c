58707a07dff0c326bed00823a9dea624edf055d2
David Wickes <dave.wickes@gmail.com>
2017-04-07 23:17:54 +0100

    still a bit of a wip

--- string-algorithms/assignment2/bwtinverse/c/main.c
+++ string-algorithms/assignment2/bwtinverse/c/main.c
@@ -3,37 +3,73 @@
 
 #define MOD 1000000001
 
-int main() {
-  char read_in[1000000];
-  int count[256] = {0};
+void sort_string(char target[], int count[256], int char_start_index[256]) {
+  int index = 0;
+  for (int c = 0; c <= 255; ++c) {
+    for (int j = 0; j < count[c]; j++) {
+      if (char_start_index[c] == -1) {
+        char_start_index[c] = index;
+      }
 
+      target[index] = c;
+      index++;
+    }
+  }
+
+  target[index] = '\0';
+}
+
+int read_in_with_char_counts(char target[], int count[256]) {
   unsigned char c = '\0';
   int bwt_length = 0;
 
   while (c != '\n') {
     c = getc(stdin);
-    read_in[bwt_length] = c;
+    if (c == '\n') break;
+
+    target[bwt_length] = c;
     count[c]++;
     bwt_length++;
   }
 
-  char bwt[bwt_length];
-  char sorted[bwt_length];
+  return bwt_length;
+}
+
+int main() {
+  char read_in[1000000];
+  int count[256];
+  int char_start_index[256];
+
+  for (int i = 0; i < 256; i++) {
+    count[i] = 0;
+    char_start_index[i] = -1;
+  }
+
+  int bwt_length = read_in_with_char_counts(read_in, count);
+
+  char bwt[bwt_length + 1];
+  char sorted[bwt_length + 1];
+  char answer[bwt_length + 1];
+  bwt[bwt_length] = '\0';
+  sorted[bwt_length] = '\0';
+  answer[bwt_length] = '\0';
 
   for (int i = bwt_length; i > -1; --i) {
     bwt[i] = read_in[i];
-    sorted[i] = read_in[i];
   }
 
-  for (int c = 0; c <= 255; ++c) {
-    int sorted_index = 0;
-    for (int j = 0; j < count[c]; ++j) {
-      sorted[sorted_index] = c;
-      sorted_index++;
-    }
+  sort_string(sorted, count, char_start_index);
+
+  answer[bwt_length - 1] = '$';
+  int pos = 0;
+
+  for (int i = 1; i < bwt_length; i++) {
+    answer[bwt_length - 1 - i] = bwt[pos];
+    unsigned char c = bwt[pos];
+    pos = char_start_index[c] + count[c];
   }
 
-  printf("%s", read_in);
-  printf("%s", bwt);
-  printf("%s", sorted);
+  printf("len: %d\n", bwt_length);
+  printf("%s\n", bwt);
+  printf("%s\n", sorted);
 }
